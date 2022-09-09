e3590ad8ea1d9c416f12f60514ed8666bbb3ddae
Maxime Debruyne <mdebruynepro@gmail.com>
2022-09-09 14:36:37 +0200

    page de connexion

---
+++ lnfb/connexion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap" rel="stylesheet"> 
+    <title>LNFB - Connexion</title>
+    <link rel="stylesheet" href="connexion.css">
+
+</head>
+<body>
+    <form action="" method="post">
+        <div class="divForm">
+
+            <div class="divinput">
+                <p class="textForm">Pseudo :</p>
+                <input type="text" id="pseudo">
+            </div>
+
+            <div class="divinput">
+                <p class="textForm">Mot de passe :</p>
+                <input type="password" id="Mots de passe">
+            </div>
+
+        </div>
+    </form>
+</body>
+</html>
